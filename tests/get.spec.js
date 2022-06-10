587f2472af73e29eb7119a79f7846a1c5d20fa39
sean evans <sean.evans@sky.uk>
2022-06-10 14:46:07 +0100

    written a test for the get request on the app

--- tests/get.spec.js
+++ tests/get.spec.js
@@ -2,3 +2,12 @@ const app = require('../src/app');
 const request = require('supertest');
 const expect = require('chai').expect;
 
+describe('get requests', () => {
+   it('get course id', () => {
+      request(app)
+          .get('/course/1')
+          .end((err, res) => {
+             expect(res.body.id).to.be.equal('1');
+          });
+   });
+});
