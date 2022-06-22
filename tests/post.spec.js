91755bd003c17abf50c3c1b1d8472abca00ff7f9
sean evans <sean.evans@sky.uk>
2022-06-22 15:49:57 +0100

    added a test for the json reponse on the post

--- tests/post.spec.js
+++ tests/post.spec.js
@@ -2,3 +2,14 @@ const app = require('../src/app');
 const request = require('supertest');
 const expect = require('chai');
 
+describe('Post requests', () => {
+    it('json response', () => {
+        request(app)
+            .post('/course')
+            .send({"name":"supertest" })
+            .set('Accept', 'application/json')
+            .end((err, res) => {
+                expect(res.body.name).to.be.equal('supertest');
+            })
+    });
+})
