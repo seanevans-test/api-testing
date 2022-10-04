d7daed9910e43fa4f315cfe690f99ba27f3f513d
sean evans <sean.evans@sky.uk>
2022-10-04 15:08:36 +0100

    added the first response test

--- tests/response.spec.js
+++ tests/response.spec.js
@@ -2,3 +2,12 @@ const app = require('../src/app');
 const request = require('supertest');
 const expect = require('chai').expect;
 
+describe('response', () =>{
+    it('json response', () => {
+        request(app)
+            .get('/course')
+            .end((err, res) => {
+                expect(res.body.id).to.be.equal('1');
+            })
+    });
+});
