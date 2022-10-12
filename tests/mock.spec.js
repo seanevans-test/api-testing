82bcdb0ddf123b634cd139d28566a2a96f0ec877
sean evans <sean.evans@sky.uk>
2022-10-12 10:12:11 +0100

    added a first mocked test

--- tests/mock.spec.js
+++ tests/mock.spec.js
@@ -1,3 +1,13 @@
 const nock = require('nock');
 const request = require('supertest');
 const expect = require('chai').expect;
+
+describe('mock response', () => {
+    beforeEach(() => {
+        nock('http://localhost:4000')
+            .get('/users')
+            .reply(200, {
+                "users": ({ "id": "1"})
+            });
+        });
+})
